070eb34ab1cb05422e5f015c7b40fbae1420c800
Rabindranath Mishra <70759712+rabindranath-mishra@users.noreply.github.com>
2026-03-15 22:44:01 +0530

    Simplify Vite config by removing tailwindcss
    Removed tailwindcss plugin and updated base path.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/srgf_sri_jagannath_trust_web/', // Must match your repo name exactly
 })
